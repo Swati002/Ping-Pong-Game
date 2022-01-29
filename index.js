02d79840a6c9059c73a814b5b014be4d18f4ca52
Swati Goel <72129763+Swati002@users.noreply.github.com>
2022-01-29 15:13:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function moveball() {
     // ball.style.top = y == true ? top + 4 + "px" : ball.style.top = top - 4 + "px";
     // ball.style.left = x == true ? left + 4 + "px" : ball.style.left = left - 4 + "px";
     if (yCheck) {
-        ball.style.top = top + 8 + "px";
+        ball.style.top = top + 16 + "px";
     } else {
-        ball.style.top = top - 8 + "px";
+        ball.style.top = top - 16 + "px";
     }
     if (xCheck) {
-        ball.style.left = left + 8 + "px";
+        ball.style.left = left + 16 + "px";
     } else {
-        ball.style.left = left - 8 + "px";
+        ball.style.left = left - 16 + "px";
     }
     requestAnimationFrame(moveball);
 }
